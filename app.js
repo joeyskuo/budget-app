4720820ab91c4d94f00318363b15b28c95cc744f
Joey <joeyskuo@gmail.com>
2020-02-03 07:57:04 -0800

    added budgetController to app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+// budget module
+        // contains all methods with private scope
+
+var budgetController = (function() {
+
+    var x = 23;
+
+    var add = function(a) {
+        return x + a;
+    }
+
+    return {
+        publicTest: function(b) {
+            console.log(add(b));
+        }
+    }
+
+})();
